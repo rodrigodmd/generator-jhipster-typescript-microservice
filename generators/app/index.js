98207dfab88d5de6ca62facb8dfeda216d2786cc
Rodrigo <rodri@MacBook-Air-de-Rodrigo.local>
2018-04-25 23:33:16 -0300

    Modified code generation to be cleaner

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,8 +2,30 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
+const mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument('myapp', { type: String, required: false });
+    this.option('yarn', {
+      description: 'Use Yarn as the package manager',
+    });
+    this.option('docker', {
+      description: 'Install Docker artifacts including a Dockerfile',
+    });
+    this.props = {
+      useYarn : this.options.yarn,
+      docker : this.options.docker,
+      name : this.options.microerviceName || 'myapp',
+      description : 'My cool TypeScript microservice',
+      port : '3001',
+      version : '1.0.0',
+      apiRoot : '/api/v1',
+    };
+    
+  }
+
   prompting() {
     // Have Yeoman greet the user.
     this.log(
@@ -12,11 +34,35 @@ module.exports = class extends Generator {
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
+        type: 'input',
+        name: 'name',
+        message: `What's the microservice name?`,
+        default: this.props.name
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: `What's the microservice description?`,
+        default: this.props.description
+      },
+      {
+        type: 'input',
+        name: 'port',
+        message: `What port should the microservice use?`,
+        default: this.props.port
+      },
+      {
+        type: 'input',
+        name: 'apiRoot',
+        message: `What's th API Root?`,
+        default: this.props.apiRoot
+      },
+      {
+        type: 'input',
+        name: 'apiVersion',
+        message: `What's the API Version?`,
+        default: this.props.version
+      },
     ];
 
     return this.prompt(prompts).then(props => {
@@ -26,13 +72,46 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.log("");
+    this.log(chalk.green(`Generating code...`));
+    mkdirp.sync(this.props.name);
+    this.fs.copyTpl(
+      this.templatePath(),
+      this.destinationPath(this.props.name), {
+          name: this.props.name,
+          title: this.props.name,
+          description: this.props.description,
+          port: this.props.port,
+          apiRoot: this.props.apiRoot,
+          version: this.props.version,
+      }
+    );
+
+    // Copy all dotfiles
+    this.fs.copyTpl(
+      this.templatePath('./.*'),
+      this.destinationRoot(this.props.name + "/"), {
+        name: this.props.name,
+        port: this.props.port,
+      }
     );
   }
 
   install() {
-    this.installDependencies();
+    this.log("");
+    this.log(chalk.green(`Resolving dependencies...`));
+    if (this.props.useYarn) {
+      this.yarnInstall();
+    } else {
+      this.npmInstall();
+    }
+    this.config.save();
+  }
+
+  end() {
+    this.log("");
+    this.log(chalk.green("Now it's time to play:"));
+    this.log(chalk.green(`        cd ${this.props.name}`));
+    this.log(chalk.green(`        npm run dev`));
   }
 };
---
+++ generators/app/templates/README.md
@@ -0,0 +1,34 @@
+# <%= name %>
+
+<%= description %>
+
+## Install It
+```
+npm install
+```
+
+## Run It
+#### Run in *development* mode:
+
+```
+npm run dev
+```
+
+#### Run in *production* mode:
+
+```
+npm run compile
+npm start
+```
+
+#### Deploy to the Cloud
+e.g. CloudFoundry
+
+```
+cf push <%= name %>
+```
+
+### Try It
+* Point you're browser to [http://localhost:3000](http://localhost:3000)
+* Invoke the example REST endpoint `curl http://localhost:3000<%= apiRoot %>/examples`
+   
